afb5ca750b1cf2a29c0f5020e22cb3886ca80c0c
HartwigH <hartwig.hii@gmail.com>
2023-06-14 10:40:26 +0300

    feat(methods): lint, notification url change

--- src/methods.ts
+++ src/methods.ts
@@ -1,7 +1,13 @@
-import jwt from 'jsonwebtoken';
-import axios from 'axios';
-import { config } from './config';
-import { BankList, CountryCode, Credentials, PayloadOpts, UserOpts } from './types';
+import jwt from "jsonwebtoken";
+import axios from "axios";
+import { config } from "./config";
+import {
+  BankList,
+  CountryCode,
+  Credentials,
+  PayloadOpts,
+  UserOpts,
+} from "./types";
 export const makePaymentUrl = (
   { secrectKey, accessKey, sandbox }: Credentials,
   {
@@ -9,22 +15,23 @@ export const makePaymentUrl = (
     currency,
     merchant_reference,
     merchant_return_url,
-    preselected_aspsp
+    merchant_notification_url,
+    preselected_aspsp,
   }: UserOpts
 ): string => {
-  const baseUrl = sandbox ? config.sandboxUrl : config.productionUrl
+  const baseUrl = sandbox ? config.sandboxUrl : config.productionUrl;
   const payload: PayloadOpts = {
     amount: amount,
-    currency: currency ? currency : 'EUR',
+    currency: currency ? currency : "EUR",
     merchant_reference: merchant_reference,
     access_key: accessKey,
     merchant_return_url: merchant_return_url,
-    merchant_notification_url: merchant_return_url,
-    preselected_aspsp: preselected_aspsp ? preselected_aspsp : ''
+    merchant_notification_url: merchant_notification_url,
+    preselected_aspsp: preselected_aspsp ? preselected_aspsp : "",
   };
   const token = jwt.sign(payload, secrectKey, {
-    algorithm: 'HS256',
-    expiresIn: '10m',
+    algorithm: "HS256",
+    expiresIn: "10m",
   });
   const paymentUrl = `${baseUrl}?payment_token=${token}`;
   return paymentUrl;
@@ -35,22 +42,25 @@ export const makePaymentUrl = (
  * Pass a country_code to get banks for specific country e.g 'FI' | 'EE' | 'LV' | 'LT'
  * @returns List of available banks for a merchant.
  */
-export const getMethods = async ({accessKey, secrectKey, sandbox}:Credentials, lang:CountryCode) => {
-  const baseUrl = sandbox ? config.sandboxMethods : config.prodcutionMethods
+export const getMethods = async (
+  { accessKey, secrectKey, sandbox }: Credentials,
+  lang: CountryCode
+) => {
+  const baseUrl = sandbox ? config.sandboxMethods : config.prodcutionMethods;
   try {
     const payload = {
-      access_key: accessKey
-  }
-  const token = jwt.sign(
-    payload, 
-    secrectKey,
-    { algorithm: 'HS256', expiresIn: '1h' }
-  );
-  const resp = await axios(baseUrl, {headers: {'Authorization': `Bearer ${token}`}})
-  if(!lang) return resp.data
-  return resp.data[lang]
+      access_key: accessKey,
+    };
+    const token = jwt.sign(payload, secrectKey, {
+      algorithm: "HS256",
+      expiresIn: "1h",
+    });
+    const resp = await axios(baseUrl, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    if (!lang) return resp.data;
+    return resp.data[lang];
   } catch (error) {
-  return error
+    return error;
   }
-}
-
+};
